b9ff1779356165609eaf7461a488232d03134c6e
Miguel <migsilvaz1@gmail.com>
2021-10-27 08:58:25 +0200

    Inicital modules

---
+++ liferay-workspace/modules/taskmanager-portlet/src/main/java/taskmanager/portlet/constants/TaskmanagerPortletKeys.java
@@ -0,0 +1,11 @@
+package taskmanager.portlet.constants;
+
+/**
+ * @author miguel.silva
+ */
+public class TaskmanagerPortletKeys {
+
+	public static final String TASKMANAGER =
+		"taskmanager_portlet_TaskmanagerPortlet";
+
+}
